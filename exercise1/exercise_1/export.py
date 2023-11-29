52b241c8b2dcd6c82758f70176335589f8447886
Berfin Kavsut <berfin.kavsut@tum.de>
2023-11-29 21:10:50 +0100

    finished hw

--- exercise1/exercise_1/export.py
+++ exercise1/exercise_1/export.py
@@ -14,16 +14,23 @@ def export_mesh_to_obj(path, vertices, faces):
     # write faces starting with "f "
 
     # ###############
-    faces = faces + 1
+    # faces = faces + 1
     with open(path, 'w') as file:
         for vertex in vertices:
             v_str = 'v' + ' ' + str(vertex[0]) + ' ' + str(vertex[1]) + ' ' + str(vertex[2]) + ' \n'
             file.write(v_str)
-
         for face in faces:
             f_str = 'f' + ' ' + str(face[0]) + ' ' + str(face[1]) + ' ' + str(face[2]) + ' \n'
             file.write(f_str)
-    return
+
+    """file_str = ''
+    for vertex in vertices:
+        file_str += f'v {vertex[0]} {vertex[1]} {vertex[2]}\n'
+    for face in faces:
+        file_str += f'f {face[0]} {face[1]} {face[2]}\n'
+
+    with open(path, 'w') as f:
+        f.write(file_str)"""
     # ###############
 
 
@@ -36,6 +43,8 @@ def export_pointcloud_to_obj(path, pointcloud):
     """
 
     # ###############
-    # TODO: Implement
-    raise NotImplementedError
+    with open(path, 'w') as file:
+        for point in pointcloud:
+            p_str = 'v' + ' ' + str(point[0]) + ' ' + str(point[1]) + ' ' + str(point[2]) + ' \n'
+            file.write(p_str)
     # ###############
--- exercise1/exercise_1/point_cloud.py
+++ exercise1/exercise_1/point_cloud.py
@@ -12,37 +12,36 @@ def sample_point_cloud(vertices, faces, n_points):
     """
 
     # ###############
-    total_area = 0
+    areas = []
+    face_num = len(faces)
     for idx, face in enumerate(faces):
         A, B, C = vertices[face]
         edge_vector_1 = B - A
         edge_vector_2 = C - A
         triangle_area = 0.5 * np.linalg.norm(np.cross(edge_vector_1, edge_vector_2))
-        total_area += triangle_area
+        areas.append(triangle_area)
+
+    probs = [area / np.sum(areas) for area in areas]
+
+    selected_indices = np.random.choice(len(faces), size=n_points, p=probs)
 
     points = np.zeros(shape=(n_points, 3))
-    total_n = 0
-    for idx, face in enumerate(faces):
-        A, B, C = vertices[face]
 
-        edge_vector_1 = B - A
-        edge_vector_2 = C - A
-        triangle_area = 0.5 * np.linalg.norm(np.cross(edge_vector_1, edge_vector_2))
+    for i, idx in enumerate(selected_indices):
+        v_ind = faces[idx]
+        A, B, C = vertices[v_ind[0]], vertices[v_ind[1]], vertices[v_ind[2]]
 
-        # TODO: wrong
-        prob = (triangle_area / total_area)
-        n = int(prob * n_points)
-        total_n = total_n + n
-
-        r1 = np.random.rand(n)
-        r2 = np.random.rand(n)
+        # Generate random indices based on the probabilities
+        r1 = np.random.rand(1)
+        r2 = np.random.rand(1)
 
         u = 1 - np.sqrt(r1)
         v = np.sqrt(r1) * (1-r2)
         w = np.sqrt(r1) * r2
 
-        P = u * A[:, np.newaxis] + v * B[:, np.newaxis] + w * C[:, np.newaxis]
-        points[idx] = np.array(P.reshape((3, 1)))
+        P = u[:, np.newaxis] * A + v[:, np.newaxis] * B + w[:, np.newaxis] * C
+        # P = u * A[:, np.newaxis] + v * B[:, np.newaxis] + w * C[:, np.newaxis]
+        points[i] = np.array(P)
 
     return points
     # ###############
