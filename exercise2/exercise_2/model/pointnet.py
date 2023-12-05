95a5267cc8736877c49202fad3d49d9fa7fc148b
Berfin Kavsut <berfin.kavsut@tum.de>
2023-12-06 00:56:33 +0100

    added training code for PointNetClassification, minor issues with forward

--- exercise2/exercise_2/model/pointnet.py
+++ exercise2/exercise_2/model/pointnet.py
@@ -10,41 +10,22 @@ class TNet(nn.Module):
 
         ########################################################################
         # Convolutional k->64, 64->128, 128->1024 with corresponding batch norms and ReLU
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(in_channels=k, out_channels=64, kernel_size=1),
-            nn.BatchNorm1d(64),
-            nn.ReLU()
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1),
-            nn.BatchNorm1d(128),
-            nn.ReLU()
-        )
-
-        self.conv3 = nn.Sequential(
-            nn.Conv1d(in_channels=128, out_channels=1024, kernel_size=1),
-            nn.BatchNorm1d(1024),
-            nn.ReLU()
-        )
+        self.conv1 = nn.Conv1d(in_channels=k, out_channels=64, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1)
+        self.conv3 = nn.Conv1d(in_channels=128, out_channels=1024, kernel_size=1)
 
         # Linear 1024->512, 512->256, 256->k^2 with corresponding batch norms and ReLU
-        self.linear4 = nn.Sequential(
-            nn.Linear(in_features=1024, out_features=512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-        )
+        self.fc1 = nn.Linear(in_features=1024, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=256)
+        self.fc3 = nn.Linear(in_features=256, out_features=k ** 2)
 
-        self.linear5 = nn.Sequential(
-            nn.Linear(in_features=512, out_features=256),
-            nn.BatchNorm1d(256),
-            nn.ReLU(),
-        )
+        self.relu = nn.ReLU()
 
-        self.linear6 = nn.Sequential(
-            nn.Linear(in_features=256, out_features=k ** 2),
-        )
-
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
         ########################################################################
         self.register_buffer('identity', torch.from_numpy(np.eye(k).flatten().astype(np.float32)).view(1, k ** 2))
         self.k = k
@@ -57,18 +38,18 @@ class TNet(nn.Module):
         # No batch norm and relu after the last Linear layer
         ########################################################################
         # Convolutional layers
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
 
         # Max operation
         x = torch.max(x, 2, keepdim=True)[0]
         x = x.view(-1, 1024)
 
         # Linear layers
-        x = self.linear4(x)
-        x = self.linear5(x)
-        x = self.linear6(x)
+        x = self.relu(self.bn4(self.fc1(x)))
+        x = self.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
         ########################################################################
 
         # Adding the identity to constrain the feature transformation matrix to be close to orthogonal matrix
@@ -77,25 +58,26 @@ class TNet(nn.Module):
         x = x.view(-1, self.k, self.k)
         return x
 
+
 class PointNetEncoder(nn.Module):
     def __init__(self, return_point_features=False):
         super().__init__()
 
         ########################################################################
-        self.input_transform_net = TNet(k=3)
-
-        self.feature_transform_net = TNet(k=64)
-
         # Define convolution layers, batch norm layers, and ReLU
-        self.conv1 = nn.Conv1d(in_channels=3, out_channels=64)
-        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128)
-        self.conv3 = nn.Conv1d(in_channels=128, out_channels=1024)
+        self.conv1 = nn.Conv1d(in_channels=3, out_channels=64, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1)
+        self.conv3 = nn.Conv1d(in_channels=128, out_channels=1024, kernel_size=1)
 
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(1024)
 
         self.relu = nn.ReLU()
+
+        self.input_transform_net = TNet(k=3)
+
+        self.feature_transform_net = TNet(k=64)
         ########################################################################
 
         self.return_point_features = return_point_features
@@ -117,7 +99,7 @@ class PointNetEncoder(nn.Module):
 
         ########################################################################
         # Layers 2 and 3: 64->128, 128->1024
-        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn2(self.conv2(point_features)))
         x = self.relu(self.bn3(self.conv3(x)))
         ########################################################################
 
@@ -130,6 +112,7 @@ class PointNetEncoder(nn.Module):
             return torch.cat([x, point_features], dim=1)
         else:
             return x
+
 
 class PointNetClassification(nn.Module):
     def __init__(self, num_classes):
@@ -146,10 +129,10 @@ class PointNetClassification(nn.Module):
         self.fc2 = nn.Linear(in_features=512, out_features=256)
         self.fc3 = nn.Linear(in_features=256, out_features=num_classes)
 
+        self.dropout = nn.Dropout(p=0.3)
+
         self.bn1 = nn.BatchNorm1d(512)
         self.bn2 = nn.BatchNorm1d(256)
-
-        self.dropout = nn.Dropout(p=0.3)
 
         self.relu = nn.ReLU()
         ########################################################################
