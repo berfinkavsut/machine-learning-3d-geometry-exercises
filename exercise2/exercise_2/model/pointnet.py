2b615eb7ac5998a0c50aac17bbf96bdd79e61fbd
Berfin Kavsut <berfin.kavsut@tum.de>
2023-12-06 01:28:57 +0100

    minor changes after checking steps from notebook

--- exercise2/exercise_2/model/pointnet.py
+++ exercise2/exercise_2/model/pointnet.py
@@ -10,9 +10,9 @@ class TNet(nn.Module):
 
         ########################################################################
         # Convolutional k->64, 64->128, 128->1024 with corresponding batch norms and ReLU
-        self.conv1 = nn.Conv1d(in_channels=k, out_channels=64, kernel_size=1)
-        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1)
-        self.conv3 = nn.Conv1d(in_channels=128, out_channels=1024, kernel_size=1)
+        self.conv1 = nn.Conv1d(in_channels=k, out_channels=64, kernel_size=1, stride=1)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1, stride=1)
+        self.conv3 = nn.Conv1d(in_channels=128, out_channels=1024, kernel_size=1, stride=1)
 
         # Linear 1024->512, 512->256, 256->k^2 with corresponding batch norms and ReLU
         self.fc1 = nn.Linear(in_features=1024, out_features=512)
@@ -99,8 +99,8 @@ class PointNetEncoder(nn.Module):
 
         ########################################################################
         # Layers 2 and 3: 64->128, 128->1024
-        x = self.relu(self.bn2(self.conv2(point_features)))
-        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
         ########################################################################
 
         # This is the symmetric max operation
@@ -143,8 +143,9 @@ class PointNetClassification(nn.Module):
         # Pass output of encoder through your layers
         ########################################################################
         x = self.relu(self.bn1(self.fc1(x)))
-        x = self.relu(self.bn2(self.fc2(x)))
+        x = self.fc2(x)
         x = self.dropout(x)
+        x = self.relu(self.bn2(x))
         x = self.fc3(x)
         ########################################################################
         return x
@@ -155,10 +156,31 @@ class PointNetSegmentation(nn.Module):
         super().__init__()
         self.num_classes = num_classes
         self.encoder = PointNetEncoder(return_point_features=True)
-        # TODO: Define convolutions, batch norms, and ReLU
+        ########################################################################
+        # Define convolutions, batch norms, and ReLU
+        ########################################################################
+        self.conv1 = nn.Conv1d(in_channels=1088, out_channels=512, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=512, out_channels=256, kernel_size=1)
+        self.conv3 = nn.Conv1d(in_channels=256, out_channels=128, kernel_size=1)
+        self.conv4 = nn.Conv1d(in_channels=128, out_channels=num_classes, kernel_size=1)
+
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.bn3 = nn.BatchNorm1d(128)
+
+        self.relu = nn.ReLU()
+        ########################################################################
 
     def forward(self, x):
         x = self.encoder(x)
-        # TODO: Pass x through all layers, no batch norm or ReLU after the last conv layer
+        ########################################################################
+        # Pass x through all layers, no batch norm or ReLU after the last conv layer
+        ########################################################################
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.conv4(x)
+        ########################################################################
+
         x = x.transpose(2, 1).contiguous()
         return x
