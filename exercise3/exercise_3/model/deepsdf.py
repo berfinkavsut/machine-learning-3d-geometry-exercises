1d148789b5a0f5227c0b2d694dcfa69bc6c1c00c
berfinkavsut <berfinkavsut@gmail.com>
2023-12-15 11:47:56 +0100

    added deepsdf model

--- exercise3/exercise_3/model/deepsdf.py
+++ exercise3/exercise_3/model/deepsdf.py
@@ -10,13 +10,42 @@ class DeepSDFDecoder(nn.Module):
         """
         super().__init__()
         dropout_prob = 0.2
+        ###############################################################################
+        # TODO better way to add weight norms?
+        # TODO model parameter numbers are not exactly
+        self.fc0 = torch.nn.utils.weight_norm(nn.Linear(in_features=(latent_size+3), out_features=512))
+        self.fc1 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=512))
+        self.fc2 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=512))
+        self.fc3 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=(latent_size-3)))
+        self.fc4 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=512))
+        self.fc5 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=512))
+        self.fc6 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=512))
+        self.fc7 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=512))
+        self.fc8 = torch.nn.utils.weight_norm(nn.Linear(in_features=512, out_features=1))
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout_prob)
+        ###############################################################################
 
-        # TODO: Define model
 
     def forward(self, x_in):
         """
         :param x_in: B x (latent_size + 3) tensor
         :return: B x 1 tensor
         """
-        # TODO: implement forward pass
+        ###############################################################################
+        x = self.dropout(self.relu(self.fc0(x_in)))
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.dropout(self.relu(self.fc2(x)))
+        x = self.dropout(self.relu(self.fc3(x)))
+
+        x = torch.cat([x, x_in], dim=1)
+
+        x = self.dropout(self.relu(self.fc4(x)))
+        x = self.dropout(self.relu(self.fc5(x)))
+        x = self.dropout(self.relu(self.fc6(x)))
+        x = self.dropout(self.relu(self.fc7(x)))
+        x = self.fc8(x)  # no additional layer after the last linear layer
+
+        ###############################################################################
+
         return x
